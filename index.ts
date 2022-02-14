f5fd6ba04c574634035877ea922ba6daa799a6b2
AdrianChojnacki <chojnackiadrian1@gmail.com>
2022-02-14 19:59:42 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-const example: Array<number> = [1, 2, 3, 4];
+interface Point {
+  x: number;
+  y: number;
+}
+
+const p1: Point = {
+  x: 5,
+  y: 4,
+};
+
+const p2 = {
+  x: 5,
+  y: 3,
+  z: 3,
+};
+
+const example: Point[] = [];
+
+example.push(p1);
+example.push(p2);
+
+example.map((point) => point.x);
